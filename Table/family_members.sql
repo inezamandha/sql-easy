e106b3526e82c33d1a5f42d9e07775b71babf329
Inez Amandha <inezamandhasuci@gmail.com>
2024-06-05 09:19:59 +0700

    🗃️ Add num_legs to family_members table

--- Table/family_members.sql
+++ Table/family_members.sql
@@ -3,10 +3,11 @@ CREATE TABLE family_members (
     name VARCHAR(255),
     gender VARCHAR(255),
     species VARCHAR(255),
-    num_books_read INT
+    num_books_read INT,
+    num_legs INT
 );
 
-INSERT INTO family_members (id, name, gender, species, num_books_read) VALUES
-(1, 'Dave', 'male', 'human', 200),
-(2, 'Mary', 'female', 'human', 180),
-(3, 'Pickles', 'male', 'dog', 0);
+INSERT INTO family_members (id, name, gender, species, num_books_read, num_legs) VALUES
+(1, 'Dave', 'male', 'human', 200, 2),
+(2, 'Mary', 'female', 'human', 180, 2),
+(3, 'Pickles', 'male', 'dog', 0, 4);
