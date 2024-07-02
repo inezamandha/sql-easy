d493edafcb290905f85887e60f871d727eb948a1
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-02 15:47:14 +0700

    🗃️ Add celebs_born table

---
+++ Table/celebs_born.sql
@@ -0,0 +1,10 @@
+CREATE TABLE celebs_born (
+    id INT,
+    name VARCHAR(255),
+    birthdate VARCHAR(255)
+);
+
+INSERT INTO celebs_born (id, name, birthdate) VALUES
+(1, 'Michael Jordan', '1963-02-17'),
+(2, 'Justin Timberlake', '1981-01-31'),
+(3, 'Taylor Swift', '1989-12-13');
