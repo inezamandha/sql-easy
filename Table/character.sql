8ad28eb31e786164de65ce92bc5b4a73ce9dd534
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-02 16:29:17 +0700

    🗃️ Add character table

---
+++ Table/character.sql
@@ -0,0 +1,10 @@
+CREATE TABLE character (
+    id INT,
+    name VARCHAR(255)
+);
+
+INSERT INTO character (id, name) VALUES
+(1, 'Doogie Howser'),
+(2, 'Barney Stinson'),
+(3, 'Lily Aldrin'),
+(4, 'Willow Rosenberg');
