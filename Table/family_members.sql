140c5e0dff1f4d04d6a0bf4cc13d3ddd23fd110c
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-02 15:28:35 +0700

    🗃️ Add favorite_book to family_members table

--- Table/family_members.sql
+++ Table/family_members.sql
@@ -4,10 +4,11 @@ CREATE TABLE family_members (
     gender VARCHAR(255),
     species VARCHAR(255),
     num_books_read INT,
-    num_legs INT
+    num_legs INT,
+    favorite_book VARCHAR(255)
 );
 
-INSERT INTO family_members (id, name, gender, species, num_books_read, num_legs) VALUES
-(1, 'Dave', 'male', 'human', 200, 2),
-(2, 'Mary', 'female', 'human', 180, 2),
-(3, 'Pickles', 'male', 'dog', 0, 4);
+INSERT INTO family_members (id, name, gender, species, num_books_read, num_legs, favorite_book) VALUES
+(1, 'Dave', 'male', 'human', 200, 2, 'To Kill a Mockingbird'),
+(2, 'Mary', 'female', 'human', 180, 2, 'Gone with the Wind'),
+(3, 'Pickles', 'male', 'dog', 0, 4, null);
