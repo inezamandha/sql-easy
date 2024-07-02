dd322058d8f6b479ef324d21c383b1a2270fbfdb
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-02 16:36:54 +0700

    🗃️ Add character_tv_show table

---
+++ Table/character_tv_show.sql
@@ -0,0 +1,11 @@
+CREATE TABLE character_tv_show (
+    id INT,
+    character_id INT,
+    tv_show_name VARCHAR(255)
+);
+
+INSERT INTO character_tv_show (id, name) VALUES
+(1, 4, 'Buffy the Vampire Slayer'),
+(2, 3, 'How I Met Your Mother'),
+(3, 2, 'How I Met Your Mother'),
+(4, 1, 'Doogie Howser, M.D.');
