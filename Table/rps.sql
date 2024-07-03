a228be4e36f8118440b9b7e50c6719266e43fab7
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-03 15:31:46 +0700

    🗃️ Add rps table

---
+++ Table/rps.sql
@@ -0,0 +1,10 @@
+CREATE TABLE rps (
+    id INT,
+    name VARCHAR(255),
+    defeats_id INT
+);
+
+INSERT INTO rps (id, name, defeats_id) VALUES
+(1, 'Rock', 3),
+(2, 'Paper', 1),
+(3, 'Scissors', 2);
