3b48d3df2949d7eb8de79cbbc770ea69c2b4bd36
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-03 10:52:19 +0700

    🗃️ Add tv_show table

---
+++ Table/tv_show.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tv_show (
+    id INT,
+    name VARCHAR(255)
+);
+
+INSERT INTO tv_show (id, name) VALUES
+(1, 'Buffy the Vampire Slayer'),
+(2, 'How I Met Your Mother'),
+(3, 'Doogie Howser, M.D.');
