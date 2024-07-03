f1b484bf7a3c394a726400507b575afa10292c10
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-03 10:58:03 +0700

    🗃️ Add actor_id to character_actor table

--- Table/character_actor.sql
+++ Table/character_actor.sql
@@ -1,11 +1,12 @@
 CREATE TABLE character_actor (
     id INT,
     character_id INT,
+    actor_id INT,
     actor_name VARCHAR(255)
 );
 
-INSERT INTO character_actor (id, character_id, actor_name) VALUES
-(1, 4, 'Alyson Hannigan'),
-(2, 3, 'Alyson Hannigan'),
-(3, 2, 'Neil Patrick Harris'),
-(4, 1, 'Neil Patrick Harris');
+INSERT INTO character_actor (id, character_id, actor_id, actor_name) VALUES
+(1, 4, 1, 'Alyson Hannigan'),
+(2, 3, 1, 'Alyson Hannigan'),
+(3, 2, 2, 'Neil Patrick Harris'),
+(4, 1, 2, 'Neil Patrick Harris');
