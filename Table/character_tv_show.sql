6e252be74afda533859a66100d3295247f97333b
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-03 10:54:25 +0700

    🗃️ Add tv_show_id to character_tv_show table

--- Table/character_tv_show.sql
+++ Table/character_tv_show.sql
@@ -1,11 +1,12 @@
 CREATE TABLE character_tv_show (
     id INT,
     character_id INT,
+    tv_show_id INT,
     tv_show_name VARCHAR(255)
 );
 
-INSERT INTO character_tv_show (id, character_id, tv_show_name) VALUES
-(1, 4, 'Buffy the Vampire Slayer'),
-(2, 3, 'How I Met Your Mother'),
-(3, 2, 'How I Met Your Mother'),
-(4, 1, 'Doogie Howser, M.D.');
+INSERT INTO character_tv_show (id, character_id, tv_show_id, tv_show_name) VALUES
+(1, 4, 3, 'Buffy the Vampire Slayer'),
+(2, 3, 2, 'How I Met Your Mother'),
+(3, 2, 2, 'How I Met Your Mother'),
+(4, 1, 1, 'Doogie Howser, M.D.');
