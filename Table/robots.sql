db8ca5014899c3b96e64564e71382f9b297777fc
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-03 16:32:25 +0700

    🗃️ Add location to robots table

--- Table/robots.sql
+++ Table/robots.sql
@@ -1,14 +1,15 @@
 CREATE TABLE robots (
     id INT,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    location VARCHAR(255)
 );
 
-INSERT INTO robots (id, name) VALUES
-(1, 'Robot 2000'),
-(2, 'Champion Robot 2001'),
-(3, 'Dragon'),
-(4, 'Turbo Robot 2002'),
-(5, 'Super Robot 2003'),
-(6, 'Super Turbo Robot 2004'),
-(7, 'Not A Robot'),
-(8, 'Unreleased Turbo Robot 2111');
+INSERT INTO robots (id, name, location) VALUES
+(1, 'Robot 2000', 'New City, NY'),
+(2, 'Champion Robot 2001', 'Palo Alto, CA'),
+(3, 'Dragon', 'New York City'),
+(4, 'Turbo Robot 2002', 'Spring Valley, NY'),
+(5, 'Super Robot 2003', 'Nyack, NY'),
+(6, 'Super Turbo Robot 2004', 'Tampa, FL'),
+(7, 'Not A Robot', 'Seattle, WA'),
+(8, 'Unreleased Turbo Robot 2111', 'Buffalo, NY');
