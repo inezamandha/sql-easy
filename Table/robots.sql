8a7ec2e3c8a3ae2f8d30926bb37cbe53b5b9b6f4
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-03 16:01:46 +0700

    🗃️ Add robots table

---
+++ Table/robots.sql
@@ -0,0 +1,14 @@
+CREATE TABLE robots (
+    id INT,
+    name VARCHAR(255)
+);
+
+INSERT INTO robots (id, name) VALUES
+(1, 'Robot 2000'),
+(2, 'Champion Robot 2001'),
+(3, 'Dragon'),
+(4, 'Turbo Robot 2002'),
+(5, 'Super Robot 2003'),
+(6, 'Super Turbo Robot 2004'),
+(7, 'Not A Robot'),
+(8, 'Unreleased Turbo Robot 2111');
